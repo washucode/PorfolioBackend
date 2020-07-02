5e8329e29d56c34c8a6530a52172206cc9318df1
washucode <estherw8525@gmail.com>
2020-07-02 12:04:08 +0300

    add models Projects,TechUsed and Skills

--- myportapp/models.py
+++ myportapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=100)
+    screenshot = models.ImageField(upload_to='projects/')
+    project_link = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.project_name
+
+
+class TechUsed(models.Model):
+    project = models.ForeignKey(Project, related_name="techused",on_delete=models.CASCADE)
+    tech_name = models.TextField()
+
+
+    def __str__(self):
+        return self.tech_name
+
+
+
+    
+
+class Skills(models.Model):
+    skill_name = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to='skills/')
+    description= models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.skill_name
+
+
